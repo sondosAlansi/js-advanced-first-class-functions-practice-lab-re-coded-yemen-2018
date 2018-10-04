37d8e2832ca22b2f5c0cef1d7d7735d08cfeb8be
sondos alansi <sondos.alansi@yahoo.com>
2018-10-04 19:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function logDriverNames(driver){
+  const cb=function(el, i, driver){
+    
+    console.log(el.name);
+  }
+  
+  driver.forEach(cb);
+  
+}
+function logDriversByHometown(driver,location){
+  const cb=function(el, i, driver){
+    if(el.hometown===location){
+    console.log(el.name);
+    }
+  }
+  
+  driver.forEach(cb);
+}
+const driversByRevenue=function (driver){
+ let arr=[];
+ arr=driver.slice();
+return arr.sort(function(a,b){
+   
+   return a.revenue - b.revenue;
+  
+ });
+}
+ 
+ const driversByName=function (driver){
+ let arr=[];
+ arr=driver.slice();
+return arr.sort(function(a,b){
+   
+   return a.name.localeCompare(b.name);
+  
+ });
+ 
+ }
+ function 
